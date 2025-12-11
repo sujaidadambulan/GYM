8a8135468284effc22dbfaea31d500f4392abf1b
sujaidadambulan <sujaidadambulan@gmail.com>
2025-12-11 15:53:58 +0530

    first messgae

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+/* =============== SHOW MENU =============== */
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/* =============== REMOVE MENU MOBILE =============== */
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* =============== CHANGE BACKGROUND HEADER =============== */
+function scrollHeader() {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/* =============== SCROLL REVEAL ANIMATION =============== */
+// Will implement intersection observer here later for sections
+const observerOptions = {
+    threshold: 0.2
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show-section');
+        }
+    });
+}, observerOptions);
+
+const sections = document.querySelectorAll('section');
+sections.forEach(section => {
+    observer.observe(section);
+});
